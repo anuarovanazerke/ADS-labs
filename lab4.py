9c053f44f20f7e80b5ea9d51b15f6a5e60a85867
Nazerke Anuarova <nazerke.anuarova@icloud.com>
2025-10-14 07:57:22 +0500

    Lab 4

--- lab4.py
+++ lab4.py
@@ -1,4 +1,39 @@
 #A
+import sys
+input=sys.stdin.readline
+def solve():
+    n,m=map(int,input().split())
+    arr=list(map(int,input().split()))
+    paths=[input().strip() for r in range(m)]
+    tree=[[val,None,None] for val in arr]
+    root=0
+    for i in range(1,n):
+        node=root
+        while True:
+            if arr[i]<tree[node][0]:
+                if tree[node][1] is None:
+                    tree[node][1]=i
+                    break
+                node=tree[node][1]
+            else:
+                if tree[node][2] is None:
+                    tree[node][2]=i
+                    break
+                node=tree[node][2]
+    for path in paths:
+        node=root
+        ok=True
+        for c in path:
+            if node is None:
+                ok=False
+
+                break
+            node=tree[node][1] if c=='L' else tree[node][2]
+        if node is None:
+            ok=False
+        print("YES" if ok else "NO")
+if __name__=="__main__":
+    solve()
 #B
 class Node:
     def __init__(self, val):
@@ -112,3 +147,20 @@ print(count_triangles(root))
 #H
 #I
 #J
+def build_order(sorted_arr):
+    n = len(sorted_arr)
+    if n == 0:
+        return []
+    mid = n // 2
+    return [sorted_arr[mid]] + build_order(sorted_arr[:mid]) + build_order(sorted_arr[mid+1:])
+
+def main():
+    k = int(input().strip())
+    n = (1 << k) - 1  # 2^k - 1
+    arr = list(map(int, input().split()))
+    arr.sort()
+    order = build_order(arr)
+    print(' '.join(map(str, order)))
+
+if __name__ == "__main__":
+    main()
